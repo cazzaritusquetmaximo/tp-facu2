f6679c1feedc37079b4729be91234d9d9a08816d
Max <cazzaritusquetmaximo@gmail.com>
2025-11-22 03:04:33 -0300

    Update sort_bubble.py

--- visualizador/algorithms/sort_bubble.py
+++ visualizador/algorithms/sort_bubble.py
@@ -1,10 +1,10 @@
 # Contrato: init(vals), step() -> {"a": int, "b": int, "swap": bool, "done": bool}
 
-items = [] #Lista de barras 4 5 1 3 2
-n = 0 #Longitud de la lista
+items = []
+n = 0
 i = 0
 j = 0
-#ignorar comentario
+
 def init(vals):
     global items, n, i, j
     items = list(vals)
@@ -12,20 +12,34 @@ def init(vals):
     i = 0
     j = 0
 
+#items[5,4,2,1,3] , len(5)
+def step(j):
+
+    if items[j] > items[j+1]:
+    items[j],items[j+1]=items[j+1],items[j]
+
+    return {"done": True}
+
+
+
+
+
 def step():
-    # TODO:
+    # TO-DO:
     # 1) Elegir índices a y b a comparar en este micro-paso (según tu Bubble).
-    #Ejemplo Longitud 5
-    for i in range (len(items)): # 0 - 4
-        for j in range (len(items)-1): # 0 - 3
-            if items[j] > items[j+1]:
-                #swap entre (lista[j], lista[j+1])
-                #swap=True?
-                items[j],items[j+1]=items[j+1],items[j]
     # 2) Si corresponde, hacer el intercambio real en items[a], items[b] y marcar swap=True.
     # 3) Avanzar punteros (preparar el próximo paso).
     # 4) Devolver {"a": a, "b": b, "swap": swap, "done": False}.
     #
     # Cuando no queden pasos, devolvé {"done": True}.
+"""
+    for i in range (len(items)):
+        for j in range (len(items)-1):
+            if items[j] > items[j+1]:
+                #swap entre (lista[j], lista[j+1])
+                #swap=True?
+                items[j],items[j+1]=items[j+1],items[j]
+"""
 
     return {"done": True}
+
