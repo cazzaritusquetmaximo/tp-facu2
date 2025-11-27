16f93c435329218abd3750d5a141a58c5e78025b
Max <cazzaritusquetmaximo@gmail.com>
2025-11-27 15:10:11 -0300

    VERSION POSTA
    lo escribi todo de nuevo

--- visualizador/algorithms/sort_selection.py
+++ visualizador/algorithms/sort_selection.py
@@ -15,50 +15,33 @@ def init(vals):
     j = i + 1
     min_idx = i
     fase = "buscar"
-"""
+
 def step():
-    # TO-DO:
-    # - Fase "buscar": comparar j con min_idx, actualizar min_idx, avanzar j.
-    #   Devolver {"a": min_idx, "b": j_actual, "swap": False, "done": False}.
-    #   Al terminar el barrido, pasar a fase "swap".
-    # - Fase "swap": si min_idx != i, hacer ese único swap y devolverlo.
-    #   Luego avanzar i, reiniciar j=i+1 y min_idx=i, volver a "buscar".
-    #
-    # Cuando i llegue al final, devolvé {"done": True}.
-    return {"done": True}
-"""
-def step():
-#[2,8,5,3,9,4,1]
+
     global items, n, i, j, min_idx, fase
 
-    # Fase "buscar": comparar j con min_idx, actualizar min_idx, avanzar j.
-    if j<n:
+    if j<n: ##barrido
         fase="buscar"
         if items[j]<items[min_idx]:
             min_idx=j
         j+=1
-    # Devolver {"a": min_idx, "b": j_actual, "swap": False, "done": False}.
         return {"a": min_idx, "b": j, "swap": False, "done": False}
 
-    #   Al terminar el barrido, pasar a fase "swap".
-    if j>=n:
+    if j==n: ##barrido terminado, fase swap
         fase="swap"
-    # Fase "swap": si min_idx != i, hacer ese único swap y devolverlo.
         if min_idx!=i:
-            items[?]=items[?], items[?]=items[?]
+            items[i],items[min_idx]=items[min_idx],items[i]
+            i+=1
+            j=i+1
+            min_idx=i
+            fase="buscar"
             return {"a": min_idx, "b": j, "swap": True, "done": False}
+        else:
+            i+=1
+            j=i+1
+            min_idx=i
+            fase="buscar"
+            return {"a": min_idx, "b": j, "swap": False, "done": False}
 
-    # Luego avanzar i, reiniciar j=i+1 y min_idx=i, volver a "buscar".
-    if "swap"=True:
-        i+=1
-        j=i+1
-        min_idx=i
-        fase="buscar"
-
-    # Cuando i llegue al final, devolvé {"done": True}.
-    if i>=n-1:
+    if i==n-1:
         return {"a": min_idx, "b": j, "swap": False, "done": True}
-
-
-## si lo ejecuto con el visualizador solo funciona 1 vez, no ordena nada porque hay algo a lo que le estoy errando(creo que =>
-## => ademas ordena de atras para adelante), y nunca me deja "recargar" el script, me salta "no se pudo  cargar el .py"
