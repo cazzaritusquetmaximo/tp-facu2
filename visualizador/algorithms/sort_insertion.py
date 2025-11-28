e876d9c815ac9949a415cc62353204758da7a356
Max <cazzaritusquetmaximo@gmail.com>
2025-11-27 21:24:36 -0300

    1ra version INSERTION
    seguramente ni ande

--- visualizador/algorithms/sort_insertion.py
+++ visualizador/algorithms/sort_insertion.py
@@ -13,9 +13,30 @@ def init(vals):
     j = None
 
 def step():
-    # TODO:
-    # - Si i >= n: devolver {"done": True}.
+    ## TO-DO:
+    ## - Si i >= n: devolver {"done": True}.
     # - Si j es None: empezar desplazamiento para el items[i] (p.ej., j = i) y devolver un highlight sin swap.
     # - Mientras j > 0 y items[j-1] > items[j]: hacer UN swap adyacente (j-1, j) y devolverlo con swap=True.
     # - Si ya no hay que desplazar: avanzar i y setear j=None.
-    return {"done": True}
+    #return {"done": True}
+
+    # Si i >= n: devolver {"done": True}.
+    if i >= n:
+        return {"a": a, "b": b, "swap": False, "done": True}
+
+    # Si j es None: empezar desplazamiento para el items[i] (p.ej., j = i) y devolver un highlight sin swap.
+    if j==None:
+        j=i
+        return {"a": a, "b": b, "swap": False, "done": False}
+
+    # Mientras j > 0 y items[j-1] > items[j]: hacer UN swap adyacente (j-1, j) y devolverlo con swap=True.
+    while j > 0 and items[j-1] > items[j]:
+        items[j-1],items[j]=items[j],items[j-1]
+        {"a": a, "b": b, "swap": True, "done": False}
+
+    # Si ya no hay que desplazar: avanzar i y setear j=None.
+    if j<=0:
+        i+=1
+        j=None
+    ##deberia devolver algo?
+
